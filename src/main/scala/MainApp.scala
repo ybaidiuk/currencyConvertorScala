f7432d9c2421b3efa99f12c3413efa878e26965d
yevhen <y.baidiuk@gmail.com>
2017-12-04 23:44:00 +0100

    stable 1.0

--- src/main/scala/MainApp.scala
+++ src/main/scala/MainApp.scala
@@ -1,33 +1,36 @@
 import BitcoinConverter._
-import DollarConverter.{dollar2bitcoin, dollar2euro}
+import DollarConverter._
+import EuroConverter._
 import akka.actor.{ActorRef, ActorSystem}
 
 object MainApp extends App {
 
-  import EuroConverter._
-
   val system: ActorSystem = ActorSystem("mainAkka")
 
-  val printer: ActorRef = system.actorOf(Printer.props, "printerActor")
-  val bitConv: ActorRef = system.actorOf(BitcoinConverter.props(printer), "bitcoinConverter")
-  val dollarConv: ActorRef = system.actorOf(DollarConverter.props(printer), "dollarConverter")
-  val euroConv: ActorRef = system.actorOf(EuroConverter.props(printer), "euroConverter")
+  try {
+    val printer: ActorRef = system.actorOf(Printer.props, "printerActor")
+    val bitConv: ActorRef = system.actorOf(BitcoinConverter.props(printer), "bitcoinConverter")
+    val dollarConv: ActorRef = system.actorOf(DollarConverter.props(printer), "dollarConverter")
+    val euroConv: ActorRef = system.actorOf(EuroConverter.props(printer), "euroConverter")
 
-  val euro = BigDecimal("300")
-  val dollar = BigDecimal("200")
-  val bitcoin = BigDecimal("1")
+    val euro = BigDecimal("300")
+    val dollar = BigDecimal("200")
+    val bitcoin = BigDecimal("1")
 
-  //Bitcoin
-  bitConv ! bitcoin2dollar(bitcoin)
-  bitConv ! bitcoin2euro(bitcoin)
+    //Bitcoin
+    bitConv ! bitcoin2dollar(bitcoin)
+    bitConv ! bitcoin2euro(bitcoin)
 
-  //Dollar
-  dollarConv ! dollar2euro(dollar)
-  dollarConv ! dollar2bitcoin(dollar)
+    //Dollar
+    dollarConv ! dollar2euro(dollar)
+    dollarConv ! dollar2bitcoin(dollar)
 
-  //Euro
-  euroConv ! euro2dollar(euro)
-  euroConv ! euro2bitcoin(euro)
+    //Euro
+    euroConv ! euro2dollar(euro)
+    euroConv ! euro2bitcoin(euro)
 
+  } finally {
+    system.terminate()
+  }
 
 }
